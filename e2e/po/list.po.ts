fc47ccc5a2f136d63b83ee0809f35a24a78aa252
Rudreshuar Galangco <skidush@gmail.com>
2019-09-15 00:28:21 +0800

    Added list page object

---
+++ e2e/po/list.po.ts
@@ -0,0 +1,18 @@
+import { by, element } from 'protractor';
+import { WebuiElement } from '../classes/classes.exports';
+import { GetElementBy } from '../helpers/helper.exports';
+import { ElementUtil } from '../utils/element.utils';
+
+export enum ListPageElement {
+  LIST_HEADER = 'cristalise-list p-table thead tr.itemCollectionRow th',
+}
+export class ListPage {
+    static _columnHeader(caption: string): Promise<WebuiElement> {
+      // For some reason, header captions are enclosed with spaces
+      return GetElementBy.cssWithExactText(element.all(by.css(ListPageElement.LIST_HEADER)), ` ${caption} `);
+    }
+
+    static get _columnHeaders() {
+      return element.all(by.css(ListPageElement.LIST_HEADER));
+    }
+}
--- e2e/po/po.exports.ts
+++ e2e/po/po.exports.ts
@@ -1,3 +1,5 @@
 export * from './login.po';
+export * from './list.po';
+export * from './details.po';
 export * from './toolbar.po';
 export * from './form.po';
