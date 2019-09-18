4a51b9ad145eb8ab3dc37ed4c0f0efbef360aade
Rudreshuar Galangco <skidush@gmail.com>
2019-09-18 23:54:38 +0800

    Moved parameter types to generic

--- project/features/support/parameterTypes.ts
+++ project/features/support/parameterTypes.ts
@@ -1,18 +1,2 @@
 import { defineParameterType } from 'cucumber'
 import { _ } from 'lodash';
-
-import { Item } from '../../../e2e/classes/item.class';
-import { ParamaterUtil } from '../../../e2e/features/support/parameterTypes';
-import { ItemSingularName, ItemPluralName } from '../../enum/generic.enum';
-
-defineParameterType({
-    regexp: ParamaterUtil.toOrFormat(ItemSingularName, true),
-    transformer: s => new Item(s),
-    name: 'item'
-});
-
-defineParameterType({
-    regexp: ParamaterUtil.toOrFormat(ItemPluralName, true),
-    transformer: s => new Item(s),
-    name: 'items'
-});
