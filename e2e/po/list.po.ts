7e4731c633a8fba8d79b21afbc43d909e05b56bb
Rudreshuar Galangco <skidush@gmail.com>
2019-09-18 23:50:35 +0800

    Added loading icon and column header elements

--- e2e/po/list.po.ts
+++ e2e/po/list.po.ts
@@ -1,17 +1,27 @@
 import { by, element } from 'protractor';
-import { WebuiElement } from '../classes/classes.exports';
+import { WebuiElement, WebuiElements } from '../classes/classes.exports';
 import { GetElementBy } from '../helpers/helper.exports';
 
 export enum ListPageElement {
-  LIST_HEADER = 'cristalise-list p-table thead tr.itemCollectionRow th',
+  LOADING_ICON = 'cristalise-list ui-table-loading',
+  HEADER = 'cristalise-list p-table thead tr.itemCollectionRow th',
+  ROWS = 'cristalise-list p-table tbody tr'
 }
 export class ListPage {
-    static _columnHeader(caption: string): Promise<WebuiElement> {
-      // For some reason, header captions are enclosed with spaces
-      return GetElementBy.cssWithExactText(element.all(by.css(ListPageElement.LIST_HEADER)), ` ${caption} `);
-    }
+  static get _loadingIcon(): Promise<WebuiElement> {
+    return GetElementBy.elementFinder(element(by.css(ListPageElement.LOADING_ICON)));
+  }
 
-    static get _columnHeaders() {
-      return element.all(by.css(ListPageElement.LIST_HEADER));
-    }
+  static _columnHeader(caption: string): Promise<WebuiElement> {
+    // For some reason, header captions are enclosed with spaces
+    return GetElementBy.cssWithExactText(element.all(by.css(ListPageElement.HEADER)), ` ${caption} `);
+  }
+
+  static get _columnHeaders(): Promise<WebuiElements> {
+    return GetElementBy.elementsFinder(element.all(by.css(ListPageElement.HEADER)));
+  }
+
+  static get _tableRows(): Promise<WebuiElements> {
+    return GetElementBy.elementsFinder(element.all(by.css(ListPageElement.ROWS)));
+  }
 }
