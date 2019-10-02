5dbdf33c9ecb8bc1e02714de0daf20a6c051258d
Rudreshuar Galangco <skidush@gmail.com>
2019-10-03 00:05:40 +0800

    Removed most of GetElementBy methods since they were moved to WebuiElement

--- e2e/helpers/element_helpers/getElement.helper.ts
+++ e2e/helpers/element_helpers/getElement.helper.ts
@@ -1,51 +1,10 @@
-import { ElementFinder, element, by, ElementArrayFinder } from "protractor";
+import { ElementArrayFinder } from "protractor";
 
-import { ElementToBe } from '../helper.exports';
-import { WebuiElement, WebuiElements } from '../../classes/classes.exports';
+import { WebuiElement } from '../../classes/classes.exports';
+import { Application, ElementUtil } from "../../utils/utils.exports";
 
+const log = Application.log('GetElementBy');
 export class GetElementBy {
-  /**
-   * Retrieves all elements with the specified css that contains the text
-   * 
-   * @param cssSelector the css selector of the element
-   * @param text the text contained by the css
-   * @param timeout the time to wait for the element in milliseconds
-   * 
-   * @returns a promise that represents the first element with the specified css which 
-   * contains the text wrapped in WebuiElements
-   */
-  static async allElementsContainingText(cssSelector: string, text: string, timeout?: number): Promise<WebuiElements> {
-    const _el = element.all(by.cssContainingText(cssSelector, text));
-    await ElementToBe.present(_el.first(), timeout);
-
-    return new WebuiElements(_el);
-  }
-
-  /**
-   * Returns the element wrapped as WebuiElement
-   *
-   * @param _el the ElementFinder to get
-   * @param timeout the time to wait for the element in milliseconds
-   * @returns a promise that represents the present element wrapped in WebuiElement
-   */
-  static async elementFinder(_el: ElementFinder, timeout: number = _el.browser_.allScriptsTimeout): Promise<WebuiElement> {
-    await ElementToBe.present(_el, timeout);
-    return new WebuiElement(_el);
-  }
-
-  /**
-   * Returns the element wrapped as WebuiElements
-   *
-   * @param _el the ElementArrayFinder to get
-   * @param timeout the time to wait for the element in milliseconds
-   * @returns a promise that represents the present element wrapped in WebuiElements
-   */
-  static async elementsFinder(_el: ElementArrayFinder, timeout: number = _el.browser_.allScriptsTimeout): Promise<WebuiElements> {
-    await ElementToBe.present(_el.first(), timeout);
-    return new WebuiElements(_el);
-  }
-  
-
   /**
    * Retrieves the element with the specified css and its exact text
    *
@@ -55,29 +14,22 @@ export class GetElementBy {
    * 
    * @returns a promise representing the element with the exact text wrapped in WebuiElement
    */
-  static async cssWithExactText(_el: ElementArrayFinder, text: string, timeout: number = _el.browser_.allScriptsTimeout): Promise<WebuiElement> {
-    await ElementToBe.present(_el.first(), timeout);
-    const elCount = await _el.count();
+  static async cssWithExactText($$elements: ElementArrayFinder, text: string, timeout: number = $$elements.browser_.allScriptsTimeout): Promise<WebuiElement> {
+    const selector = ElementUtil.selector($$elements);
+    log.debug(`Searching for element with the text '${text}' from '${selector}'`);
 
-    let _elFound: WebuiElement;
-    for (let index = 0; index < elCount; index++) {
-      const _webuiEl = new WebuiElement(_el.get(index));
-      await _webuiEl.scrollIntoView();
+    const whiteSpace = /\s+/g;
+    await new WebuiElement($$elements.first()).to.be.present(timeout);
+    const foundIndex = (<any> (await $$elements.getText())).findIndex(elementText => elementText.replace(whiteSpace, '') === text.replace(whiteSpace, ''));
 
-      let elText = await _webuiEl.getText();
-      elText = elText.replace(/\s+/g, '');
-      text = text.replace(/\s+/g, '');
-      
-      if (text === elText) {
-        _elFound = _webuiEl;    
-        break;
-      }
+    if (foundIndex) {
+      log.debug(`Element with text '${text}' within the elements '${selector}' was found at index ${foundIndex}`);
+      return new WebuiElement($$elements.get(foundIndex));
+    } else {
+      const errorMsg = `No element found within the elements '${selector} that contains the text '${text}'`;
+      log.error(errorMsg);
+      throw errorMsg;
     }
-
-    if (!_elFound) {
-      throw `No element found that contains the text: ${text}`;
-    }
-  
-    return _elFound;
+    
   }
 }
